b9aaa7ef6b7c5899498bfff71362e189f1baef4a
Hridyeshi <cse.18bcs2401@gmail.com>
2021-04-30 02:06:54 +0530

    Ready for GIT again

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,32 +3,32 @@ import React from "react";
 import { PieChart, Pie, Tooltip ,  } from "recharts";
 
 const data01 = [
-  { name: "Group1", studentnumber: 100 },
-  { name: "Group2", studentnumber: 95 },
-  { name: "Group3", studentnumber: 33 },
-  { name: "Group4", studentnumber: 44 },
-  { name: "Group5", studentnumber: 57 },
-  { name: "Group6", studentnumber: 68 },
-  { name: "Group7", studentnumber: 71 },
-  { name: "Group8", studentnumber: 82 },
-  { name: "Group9", studentnumber: 93 },
-  { name: "Group10", studentnumber: 84 },
-  { name: "Group11", studentnumber: 75 },
-  { name: "Group12", studentnumber: 26 },
+  { name: "College1", studentnumber: 100 },
+  { name: "College2", studentnumber: 95 },
+  { name: "College3", studentnumber: 33 },
+  { name: "College4", studentnumber: 44 },
+  { name: "College5", studentnumber: 57 },
+  { name: "College6", studentnumber: 68 },
+  { name: "College7", studentnumber: 71 },
+  { name: "College8", studentnumber: 82 },
+  { name: "College9", studentnumber: 93 },
+  { name: "College10", studentnumber: 84 },
+  { name: "College11", studentnumber: 75 },
+  { name: "College12", studentnumber: 26 },
 
-  { name: "Group13", studentnumber: 1 },
-  { name: "Group14", studentnumber: 5 },
-  { name: "Group15", studentnumber: 3 },
-  { name: "Group16", studentnumber: 4 },
-  { name: "Group17", studentnumber: 57 },
-  { name: "Group18", studentnumber: 68 },
-  { name: "Group19", studentnumber: 71 },
-  { name: "Group20", studentnumber: 82 },
-  { name: "Group21", studentnumber: 93 },
-  { name: "Group22", studentnumber: 84 },
-  { name: "Group23", studentnumber: 75 },
-  { name: "Group24", studentnumber: 26 },
-  { name: "Group25", studentnumber: 6 },
+  { name: "College13", studentnumber: 1 },
+  { name: "College14", studentnumber: 5 },
+  { name: "College15", studentnumber: 3 },
+  { name: "College16", studentnumber: 4 },
+  { name: "College17", studentnumber: 57 },
+  { name: "College18", studentnumber: 68 },
+  { name: "College19", studentnumber: 71 },
+  { name: "College20", studentnumber: 82 },
+  { name: "College21", studentnumber: 93 },
+  { name: "College22", studentnumber: 84 },
+  { name: "College23", studentnumber: 75 },
+  { name: "College24", studentnumber: 26 },
+  { name: "College25", studentnumber: 6 },
 
 ];
 
@@ -37,7 +37,7 @@ export default function App() {
     <div className="style">
     <center> <h2>Pie Chart Of Records</h2></center>
    
-    <PieChart width={2000} height={1000}>
+    <PieChart width={900} height={900}>
       <Pie width={800} height={900}
         dataKey="studentnumber"
         isAnimationActive={false}
--- react-app/src/setupProxy.js
+++ react-app/src/setupProxy.js
@@ -1,13 +1,13 @@
-const {
-    createProxyMiddleware
-  } = require('http-proxy-middleware');
+// const {
+//     createProxyMiddleware
+//   } = require('http-proxy-middleware');
   
-  module.exports = function (app) {
-    app.use(
-      '/api',
-      createProxyMiddleware({
-        target: 'http://localhost:3004',
-        changeOrigin: true,
-      })
-    );
-  };
+//   module.exports = function (app) {
+//     app.use(
+//       '/api',
+//       createProxyMiddleware({
+//         target: 'http://localhost:3004',
+//         changeOrigin: true,
+//       })
+//     );
+//   };
